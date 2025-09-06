89b145cd04698790ed4ef23f3908034e6dbd3cd6
OscarVasquezz <87917300+OscarVasquezz@users.noreply.github.com>
2025-09-06 05:38:00 -0600

    app5

--- vue/mivue.js
+++ vue/mivue.js
@@ -5,3 +5,41 @@ var app = new Vue({
     contador : 1
   }
 })
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'Usted cargó esta página el ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Aprender JavaScript' },
+      { text: 'Aprender Vue' },
+      { text: 'Construir algo increíble' },
+      { text: 'Nuevo Elemento' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hola Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
